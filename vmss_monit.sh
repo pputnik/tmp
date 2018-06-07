59472385bfec78a2a5bab6392267f01b268f461c
pputnik <you@example.com>
2018-06-07 15:01:17 +0200

    cron restart

--- vmss_monit.sh
+++ vmss_monit.sh
@@ -3,8 +3,10 @@
 
 touch /var/log/num.log
 chmod 666 /var/log/num.log
-echo "* * * * * /tmp/pinger.sh" > /var/spool/cron/nobody
 
+wget https://raw.githubusercontent.com/pputnik/tmp/master/pinger.sh
 mv pinger.sh /tmp/pinger.sh
 chmod 700 /tmp/pinger.sh
 chown nobody.nobody /tmp/pinger.sh /var/spool/cron/nobody
+echo "* * * * * /tmp/pinger.sh" > /var/spool/cron/nobody
+/etc/init.d/cron restart
