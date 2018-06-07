611e329e5d9bf5f397f345c86b077e18167ec5b0
pputnik <you@example.com>
2018-06-07 15:11:30 +0200

    user switch

--- vmss_monit.sh
+++ vmss_monit.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -vx
 
+# for Ubuntu on azure!
 
 touch /var/log/num.log
 chmod 666 /var/log/num.log
 
 wget https://raw.githubusercontent.com/pputnik/tmp/master/pinger.sh
 mv pinger.sh /tmp/pinger.sh
-chmod 700 /tmp/pinger.sh
-chown nobody.nobody /tmp/pinger.sh /var/spool/cron/nobody
-echo "* * * * * /tmp/pinger.sh" > /var/spool/cron/nobody
+chmod 755 /tmp/pinger.sh
+
+# old cron tasks will be deleted! Modify if needed
+echo "* * * * * /tmp/pinger.sh" > /var/spool/cron/crontabs/azureuser
+chown azureuser.azureuser /tmp/pinger.sh /var/spool/cron/crontabs/azureuser
 /etc/init.d/cron restart
