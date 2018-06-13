115c836821b093c4eb8cf0377049f7da0be27a77
pputnik <you@example.com>
2018-06-13 12:09:49 +0200

    pinger to postfix

--- pinger.sh
+++ pinger.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
-num=$(( ( RANDOM % 10 )  + 1 ))
-echo $num
-echo "`date '+%Y-%m-%d %H:%M:%S'` $num some text here" >> /var/log/num.log
+#num=$(( ( RANDOM % 10 )  + 1 ))
+#echo $num
+num=if [ $(/usr/bin/netstat -plnt | /usr/bin/grep ":25 " -c) -ne 2 ]; then echo 0; else echo 1; fi
+echo "`date '+%Y-%m-%d %H:%M:%S'` $num" >> /var/log/num.log
