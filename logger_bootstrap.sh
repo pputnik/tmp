a8f8184e008cde67f1ff590ae1fba1d1579ac7cf
pputnik <you@example.com>
2018-07-04 13:29:54 +0200

    pinger.php

--- logger_bootstrap.sh
+++ logger_bootstrap.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -v
 
-apt-get install munin munin-common munin-node munin-plugins-extra libwww-perl lynx apache2 -y
+apt-get install munin munin-common munin-node munin-plugins-extra libwww-perl lynx apache2 php7.0 php7.0-cgi php7.0-cli php7.0-common libapache2-mod-php7.0 -y
+
+# insert alias at 14th line
 sed '14i Alias /_munin /var/cache/munin/www' /etc/apache2/sites-enabled/000-default.conf -i
+sed '15i <Directory /var/cache/munin/www>' /etc/apache2/sites-enabled/000-default.conf -i
+sed '16i         Require all granted' /etc/apache2/sites-enabled/000-default.conf -i
+sed '17i </Directory>' /etc/apache2/sites-enabled/000-default.conf -i
+
+/usr/bin/wget --no-cache https://raw.githubusercontent.com/pputnik/tmp/master/pinger.php 
+mv pinger.php /var/www/html/
+mkdir /var/cache/munin/vmss
+chown munin.munin /var/cache/munin/vmss
+chmod 777 /var/cache/munin/vmss
+
+
 /usr/sbin/apachectl restart
 
---
+++ pinger.php
@@ -0,0 +1,32 @@
+<?php
+#echo "ok\n";
+#print_r($_GET);
+$dir = '/var/cache/munin/vmss';
+
+
+$host = sanitize($_GET['me']);
+if (strlen($host) <1) exit("incorrect hostname '$host'\n");
+
+$subj =  sanitize($_GET['subject']);
+if (strlen($subj) <1) exit("incorrect subject '$subj'\n");
+
+$status = intval($_GET['status']);
+
+echo "$host\t$subj\t$status\n";
+
+if (is_dir("$dir/$subj") === false){
+        if (mkdir("$dir/$subj", 0777, true) !== true) exit("cannot create $dir/$subj\n");       #create dire
+}
+
+if (file_put_contents("$dir/$subj/$host", "$host.value $status\n") <5 ) exit("cannot write to $dir/$subj/$host\n"); #overwrite old file
+echo "OK\n";
+
+//==================================================
+function sanitize($in){
+        $out = preg_replace ("/[^a-zA-Z0-9._-]/", '', $in);
+        $out = preg_replace("/^[\.-_]/", '', $out);   # those elements are nit allowed in the first place
+        $out = preg_replace("/[\.-_]$/", '', $out);   # those elements are nit allowed in the last place
+        return $out;
+}
+
+
